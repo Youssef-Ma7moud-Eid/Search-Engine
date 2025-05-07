4e6003dfda5854c18f7e6ecdd4e9a8dc58eaae23
Youssef mahmoud eid <ym1977@fayoum.edu.eg>
2025-05-07 19:34:18 +0300

    add file to decode urls and delete txt

--- WEB_CRAWLER_AND_PAGE_RANK/main.py
+++ WEB_CRAWLER_AND_PAGE_RANK/main.py
@@ -2,10 +2,9 @@ from WEB_CRAWLER_AND_PAGE_RANK.features.pagerank import PageRank
 from WEB_CRAWLER_AND_PAGE_RANK.features.web_crawler import WebCrawler
 
 
-# the start page => initial
 initial_links = ['https://www.dailynewsegypt.com/', 'https://www.geeksforgeeks.org/']
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
 
     Crawler = WebCrawler(max_links=1000)
